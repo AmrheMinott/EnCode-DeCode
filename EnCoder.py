e046e4ce8acdb03bc65b2a23a7271ec0a5e9e705
Amrhe Minott <60048439+AmrheMinott@users.noreply.github.com>
2020-06-19 20:04:28 -0400

    segmented codes into functions

--- EnCoder.py
+++ EnCoder.py
@@ -10,11 +10,8 @@
 '''
 
 
+import os
 
-# string via user to encrpt is entered
-userInput = input("\n\n\nEnter String to reverse: -> ")
-
-option = input("\n\n\nDo you want to endode (y - encode n - decode). Enter option: -> ")
 
 
 # encryption dictionary
@@ -49,62 +46,113 @@ encrypt = {
     "y":"$y$"
 }
 
+def encode(userInput):
+    # the string that will be used to hold the values of the the string in the final result
+    builtString = " "
 
-# the string that will be used to hold the values of the the string in the final result
-builtString = " "
-
-# if user does not enter exit as a value for the string
-# then the loop will continue
-
-# we encode here
-if option == "y":
     # converts the string to lowercase
     userInput = userInput.lower();
 
-    while userInput != "exit":
-        # iterate through each word
-        for word in userInput.split():
-            # if the word is one of our encrypted word from memory then replace it from the origanl string
-            if word in encrypt:
-                builtString += (encrypt.get(word) + " ")
-            else:
-                builtString += (" E" + word + "E ")
+    # iterate through each word
+    for word in userInput.split():
+        # if the word is one of our encrypted word from memory then replace it from the origanl string
+        if word in encrypt:
+            builtString += (encrypt.get(word) + " ")
+        else:
+            builtString += (" E" + word + "E ")
 
-        # reverse the string
-        reverse = builtString[::-1]
-        print(f"Your String reversed is \n\n-> {reverse}")
-        userInput = input("\n\n\nEnter Another String to EnCode: -> ")
-        userInput = userInput.lower();
-        builtString = " "
+    # reverse the string
+    reverse = builtString[::-1]
+    print(f"Your String reversed is \n\n-> {reverse}")
 
-else:
+        
+
+
+def decode(userInput):
     # we decode here
+    unverse = userInput.replace("E" , "")
+    unverse = unverse.replace("$" , "")
 
-    # infinite loop until user wants to leave
-    while userInput != "exit":
+    # flips the word backwards to undo reversing
+    unverse = unverse[::-1]
+    # iterate through each word
+    for key, value in encrypt.items():
+        for word in unverse.split():
+            if word == value:
+                unverse = unverse.replace(word , value)
+    
+    print(f"Your Decoded String is \n {unverse}")
+        
 
-        unverse = userInput.replace("E" , "")
-        unverse = unverse.replace("$" , "")
+def load(filename):
+    print('getcwd:      ', os.getcwd())
+    print('__file__:    ', __file__)
+    path = os.getcwd()
+    path = path + '/' + filename
+    print(path)
 
-        # flips the word backwards to undo reversing
-        unverse = unverse[::-1]
-        # iterate through each word
-        for key, value in encrypt.items():
-            for word in unverse.split():
-                if word == value:
-                    unverse = unverse.replace(word , value)
-
-        print(f"Your Decoded String is \n {unverse}")
-
-        userInput = input("\n\n\nEnter Another String to DeCode: -> ")
-        userInput = userInput.lower();
-
-
-
-
-
-
+    # in the event of an error we say the file could not be found and we return a dummy value
+    try:
+        file = open(filename, 'r')
+        read = file.read()
+        file.close()
+        return read
+    except FileNotFoundError:
+        print("file is not found try again")
+        return "DUMMY_VALUE"
+        
+    
+    
 
 
+
+def main():
+    # string via user to encrpt is entered
+    mainMenuOption = ""
+    userInput = ''
+
+    while mainMenuOption != "3":
+
+        print("Main Menu\n ")
+
+        mainMenuOption = input("strings (1) or a .txt file (2) or (3) to exit program ")
+
+        if mainMenuOption == "1":
+            while userInput != "exit":
+                userInput = input("\n\n\nEnter String: -> ")
+                if userInput == "exit":
+                    break
+                option = input("\n\n\n(E -> encode or D -> decode or exit to return to main menu). Enter option: -> ")
+                    
+                if option == "e" or option == "E":
+                    encode(userInput)
+                elif option == "d" or option == "D":
+                    decode(userInput)
+                elif option == 'exit': # not really needed tbh
+                    break
+            
+        elif mainMenuOption == "2":
+            while True:
+                print("Enter name of file below or type exit to return to main menu")
+                filename = input("Enter name of .txt file -> ")
+                if filename == "exit":
+                    break # to main menu
+                print("This is the filename you entered " + filename)
+                text = load(filename)
+                print("Information of the text file from the start is as follows")
+                print(text)
+                if text != "DUMMY_VALUE":
+                    option = input("(E -> encode or D -> decode")
+                    if option == "e" or option == "E":
+                        encode(text)
+                    elif option == "d" or option == "D":
+                        decode(text)
+                
+
+        elif mainMenuOption == "3" or mainMenuOption == "exit":
+            break # out of the entire program
+
+
+main()
 
 # end of program
